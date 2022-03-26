b2ca12a57dfba3c05cc41349233ffbe8a45932fb
LxnChan <LingXuanNing@Outlook.com>
2022-03-26 14:58:06 +0800

    Update Dict

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -47,8 +47,8 @@
 #define DEF_ROOT_PASSWORD	"admin"
 #define DEF_SMB_WORKGROUP	"WORKGROUP"
 #define DEF_TIMEZONE		"CST-8"
-#define DEF_NTP_SERVER0		"ntp1.aliyun.com"
-#define DEF_NTP_SERVER1		"2001:470:0:50::2"
+#define DEF_NTP_SERVER0		"ntp.ntsc.ac.cn"
+#define DEF_NTP_SERVER1		"time1.cloud.tencent.com"
 #ifdef SUPPORT_OPENSSL_EC
 #define DEF_HTTPS_CIPH_LIST	"ECDH+CHACHA20:ECDH+AESGCM:DH+AESGCM:DH+AES256:DH+AES:DH+3DES:RSA+AES:RSA+3DES:!ADH:!MD5:!DSS"
 #else
